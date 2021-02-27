2d9a0b597cf84954d65f527a2f539f1d96546a73
Keiya Watanabe <ohagi2149@gmail.com>
2021-02-27 11:20:56 +0900

    add: utils.tsテスト追加

---
+++ common/utils.test.ts
@@ -0,0 +1,126 @@
+import {
+  chunk,
+  shuffle,
+  range,
+  take,
+  transpose,
+  deepCopy,
+  reverse,
+} from './utils'
+
+describe('Test utils.ts', () => {
+  test('Test chunk function', () => {
+    // 正常系
+    // prettier-ignore
+    expect(chunk([0, 1, 2, 3, 4, 5], 2))
+        .toEqual([[0, 1], [2, 3], [4, 5]])
+
+    // 正常系
+    // prettier-ignore
+    expect(chunk([0, 1, 2, 3, 4, 5], 3))
+        .toEqual([[0, 1, 2], [3, 4, 5]])
+
+    // 端数が出る
+    // prettier-ignore
+    expect(chunk([0, 1, 2, 3, 4, 5], 4))
+        .toEqual([[0, 1, 2, 3], [4, 5]])
+
+    // 配列の長さより大きい数字を渡す
+    // prettier-ignore
+    expect(chunk([0, 1, 2], 4))
+        .toEqual([[0, 1, 2]])
+
+    // 1を渡す
+    // prettier-ignore
+    expect(chunk([0, 1, 2], 1))
+        .toEqual([[0], [1], [2]])
+
+    // 0を渡す
+    // prettier-ignore
+    expect(chunk([0, 1, 2], 0))
+        .toEqual([[0, 1, 2]])
+
+    // 負数を渡す
+    // prettier-ignore
+    expect(chunk([0, 1, 2], -2))
+        .toEqual([[0, 1, 2]])
+
+    // 空配列を渡す
+    // prettier-ignore
+    expect(chunk([], 2))
+        .toEqual([])
+  })
+
+  test('Test shuffle function', () => {
+    const arr = [0, 1, 2, 3]
+    const result = shuffle(arr)
+    // 別のオブジェクトかどうか
+    expect(result).not.toBe(arr)
+    // シャッフルされているかどうか
+    expect(result).not.toEqual(arr)
+  })
+
+  test('Test range function', () => {
+    // 正常系
+    expect(range(3)).toEqual([0, 1, 2])
+    // 開始数を指定
+    expect(range(3, 3)).toEqual([3, 4, 5])
+    // 0を指定
+    expect(range(0)).toEqual([])
+    // 長さに負数を指定
+    expect(range(-1)).toEqual([])
+    // 開始数に負数を指定
+    expect(range(3, -2)).toEqual([-2, -1, 0])
+    // 0を指定(開始数指定あり)
+    expect(range(0, 3)).toEqual([])
+  })
+
+  test('Test take function', () => {
+    // 正常系
+    expect(take([0, 1, 2, 3, 4, 5], 3)).toEqual([0, 1, 2])
+    // 配列数より大きい数を渡したら
+    expect(take([], 3)).toEqual([])
+    // 0を渡したら
+    expect(take([1, 2, 3], 0)).toEqual([])
+    // 負数を渡したら
+    expect(take([1, 2, 3], -1)).toEqual([])
+  })
+
+  test('Test transpose function', () => {
+    const result = transpose([
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ])
+    expect(result).toEqual([
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ])
+  })
+
+  test('Test deepCopy function', () => {
+    const arr = [0, 1, 2]
+    // 別のオブジェクトか
+    expect(deepCopy(arr)).not.toBe(arr)
+    // 同じ値か
+    expect(deepCopy(arr)).toEqual(arr)
+
+    const obj = { test: 'hello world' }
+    // 別のオブジェクトか
+    expect(deepCopy(obj)).not.toBe(obj)
+    // 同じ値か
+    expect(deepCopy(obj)).toEqual(obj)
+  })
+
+  test('Test reverse function', () => {
+    const arr = [0, 1, 2, 3, 4, 5]
+    const result = reverse(arr)
+    // 逆順になっているか
+    expect(result).toEqual([5, 4, 3, 2, 1, 0])
+    // 元の配列は変わらないか
+    expect(arr).toEqual([0, 1, 2, 3, 4, 5])
+    // 元の配列と違うオブジェクトか
+    expect(result).not.toBe(arr)
+  })
+})
--- common/utils.ts
+++ common/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * 配列を一定の要素数ごとに分割する
+ * @param array 分割する配列
+ * @param size 要素数
+ */
 export function chunk<T>(array: T[], size: number): T[][] {
   const chunked = []
   for (let element of array) {
@@ -19,10 +24,21 @@ export function shuffle<T>([...array]: T[]): T[] {
   return array
 }
 
+/**
+ * 連続した数字を持つ配列を作成する
+ * @param size 作成する配列の長さ
+ * @param startAt １つ目の要素
+ */
 export function range(size: number, startAt = 0) {
+  if (size < 0) return []
   return Array.from(Array(size), (_, k) => k).map((i) => i + startAt)
 }
 
+/**
+ * 渡された配列の先頭から指定要素数分の切り取った配列を返す
+ * @param array
+ * @param n
+ */
 export function take<T>(array: T[], n: number): T[] {
   if (n === 0) return []
   if (n > array.length) return [...array]
@@ -38,14 +54,22 @@ export function transpose<T>(array: T[][]): T[][] {
   return array[0].map((col, i) => array.map((row) => row[i]))
 }
 
-export function deepCopy<T>(array: T[]): T[] {
-  return JSON.parse(JSON.stringify(array))
+export function deepCopy<T>(obj: T): T[] {
+  return JSON.parse(JSON.stringify(obj))
 }
 
+/**
+ * 非破壊reverse
+ * @param array 反転する配列
+ */
 export function reverse<T>(array: T[]): T[] {
   return [...array].reverse()
 }
 
+/**
+ * 指定ミリ秒待つプロミスを返す
+ * @param ms
+ */
 export function wait(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
