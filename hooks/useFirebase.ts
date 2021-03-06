c782a142d5a0d526198bdbfbb1bdf09f8417f605
Keiya Watanabe <ohagi2149@gmail.com>
2021-03-06 11:02:47 +0900

    fix: firebase設定を環境変数に変更

--- hooks/useFirebase.ts
+++ hooks/useFirebase.ts
@@ -1,5 +1,14 @@
 import firebase from 'firebase'
-import firebaseConfig from '../configs/firebaseConfig.json'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
 
 export default function useFirebase() {
   function initFirebase() {
